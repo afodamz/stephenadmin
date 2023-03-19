81159da11c1995c1e224379e57c3d3757f8ccfed
afodamz <afodamz07@gmail.com>
2023-03-20 00:06:46 +0100

    get my cart

--- router/cart.js
+++ router/cart.js
@@ -7,8 +7,10 @@ const CartRoutes = Router();
 const cartServices = new CartServices(DB);
 
 CartRoutes.post('/add', authenticateUser, cartServices.addProductToCart);
+CartRoutes.post('/createorder', authenticateUser, cartServices.createOrder);
+CartRoutes.post('/myordes', authenticateUser, cartServices.getmyOrders);
+CartRoutes.post('/orders', authenticateUser, cartServices.getOrders);
 CartRoutes.get('/:cart_id', authenticateUser, cartServices.getProductsInCart);
-CartRoutes.get('/', authenticateUser, cartServices.getProductsInCart);
 CartRoutes.delete('/empty/:cart_id', authenticateUser, cartServices.emptyCart);
 CartRoutes.delete('/removeProduct/:item_id', authenticateUser, cartServices.removeProduct);
 
--- service/cartservice.js
+++ service/cartservice.js
@@ -9,7 +9,7 @@ export default class CartServices {
     }
 
     addProductToCart = (req, res, next) => {
-        const sub = req.user;
+        const { sub } = req.user;
         try {
             const { productId, numberofOrders } = req.body;
             let errors = [];
@@ -41,8 +41,49 @@ export default class CartServices {
         }
     }
 
+    createOrder = (req, res, next) => {
+        const { sub } = req.user;
+        try {
+            const { postalCode, country, city, apartment, address } = req.body;
+            // check if user has orders in cart
+            const orderQuery = `SELECT * FROM order_product WHERE userId = "${sub}" and is_deleted=0 and productId=null`;
+            this.DB.query(orderQuery, (err, orderResult) => {
+                if (err) return res.status(500).json({ error: err });
+                if (orderResult.length > 0) {
+                    const Q = `INSERT INTO orders SET ?`;
+                    const id = uuidv4();
+                    const Options = {
+                        id,
+                        userId: sub,
+                        orderId: `#${id.slice((id.length - 10), id.length)}`,
+                        dateCreated: new Date(),
+                        postalCode, country, city, apartment, address
+                    };
+                    this.DB.query(Q, Options, (err, result) => {
+                        if (err) return res.status(401).json({ msg: "Error adding cart" });
+                        const Q = `UPDATE order_product SET orderId=${id} where productId=null AND userId=${sub}`;
+                        this.DB.query(Q, (err, result) => {
+                            if (err) return res.status(401).json({ msg: "Error adding cart" });
+                            return res.status(200).json({
+                                error: false,
+                                result: Options,
+                            })
+                        });
+                    });
+                }else{
+                    return res.status(200).json({
+                        error: true,
+                        message: "You have no product in cart"
+                    })
+                }
+            });
+        } catch (error) {
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+
     updateProductsInCart = (req, res, next) => {
-        const sub = req.user;
+        const { sub } = req.user;
         const productId = req.params;
         try {
             const { numberofOrders } = req.body;
@@ -66,7 +107,7 @@ export default class CartServices {
     }
 
     getProductsInCart = (req, res) => {
-        const sub = req.user;
+        const { sub } = req.user;
         try {
             let errors = [];
             const Q = `SELECT * FROM order_product WHERE userId = "${sub}" and is_deleted=0`;
@@ -82,10 +123,72 @@ export default class CartServices {
         }
     }
 
+    getmyOrders = (req, res) => {
+        const { sub } = req.user;
+        try {
+            let errors = [];
+            const Q = `SELECT * FROM orders, order_product.*, products.* from orders WHERE userId = "${sub}" and is_deleted=0
+            inner join order_product ON order_product.orderId=orders.id 
+            inner join products on products.id=order_product.productId where products.is_deleted=false`;
+            // const Q = `SELECT * FROM orders, order_product.*, products.* from orders WHERE userId = "${sub}" and is_deleted=0
+            // inner join order_product ON order_product.orderId=orders.id 
+            // inner join products on products.id=order_product.productId where products.is_deleted=false`;
+            this.DB.query(Q, (err, result) => {
+                if (err) return res.status(500).json({ error: err });
+                return res.status(200).json({
+                    error: false,
+                    result,
+                })
+            });
+        } catch (error) {
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+
+    getmyCart = (req, res) => {
+        const { sub } = req.user;
+        try {
+            let errors = [];
+            const Q = `SELECT * FROM order_product.*, products.* from order_product WHERE userId = "${sub}" and is_deleted=0
+            inner join products on products.id=order_product.productId where products.is_deleted=0`;
+            // const Q = `SELECT * FROM orders, order_product.*, products.* from orders WHERE userId = "${sub}" and is_deleted=0
+            // inner join order_product ON order_product.orderId=orders.id 
+            // inner join products on products.id=order_product.productId where products.is_deleted=false`;
+            this.DB.query(Q, (err, result) => {
+                if (err) return res.status(500).json({ error: err });
+                return res.status(200).json({
+                    error: false,
+                    result,
+                })
+            });
+        } catch (error) {
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+
+    getOrders = (req, res) => {
+        const { sub } = req.user;
+        try {
+            let errors = [];
+            const Q = `SELECT * FROM orders, order_product.*, products.* from orders WHERE is_deleted=0
+            inner join order_product ON order_product.orderId=orders.id 
+            inner join products on products.id=order_product.productId where products.is_deleted=false`;
+            this.DB.query(Q, (err, result) => {
+                if (err) return res.status(500).json({ error: err });
+                return res.status(200).json({
+                    error: false,
+                    result,
+                })
+            });
+        } catch (error) {
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+
     removeProduct = (req, res) => {
         console.log("params", req.params.id)
         const productId = req.params;
-        const sub = req.user;
+        const { sub } = req.user;
         const Q = `UPDATE order_product SET is_seleted=1 where productId=${productId} AND userId=${sub}`;
 
         this.DB.query(Q, (err, result) => {
@@ -98,6 +201,7 @@ export default class CartServices {
     }
 
     emptyCart = (req, res) => {
+        const { sub } = req.user;
         console.log("params", req.params.id)
         const productId = req.params;
         const Q = `UPDATE order_product SET is_seleted=1 where userId=${sub}`;
